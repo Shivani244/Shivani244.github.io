187143ee46fd0e5e9d74e3340e9838065aa52d7d
Shivani244 <43355324+Shivani244@users.noreply.github.com>
2022-03-21 14:51:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const gridDisplay = document.querySelector('.grid')
-  const resultDisplay = document.getElementById(' result')
-const width = 5
-const height = 6
-let squares = []
-
-
-function createBoard() {
-    for (let i=0; i < width*height; i++){
-        square = document.createElement('div')
-        square.innerHTML = 0
-        gridDisplay.appendChild(square)
-        squares.push(square)
-   
-    }
-    generate()  
-}
-
-createBoard()
-
-//random grid
-
-function generate() {
-    let randomNumber = Math.floor(Math.random() * 30)
-     if (squares[randomNumber].innerHTML == 0 ) {
-        squares[randomNumber].innerHTML = 2
-     }
-}
+    const gridDisplay = document.querySelector('.grid')
+    const resultDisplay = document.getElementById(' result')
+  const width = 5
+  const height = 6
+  let squares = []
+  
+  
+  function createBoard() {
+      for (let i=0; i < width*height; i++){
+          square = document.createElement('div')
+          square.innerHTML = ''
+          gridDisplay.appendChild(square)
+          squares.push(square)
+     
+      }
+      generate()  
+  }
+  
+  createBoard()
 
   
-)
+  //random grid
+  
+  function generate() {
+      let randomNumber = Math.floor(Math.random() * 25)
+       if (squares[randomNumber].innerHTML == '' ) {
+          squares[randomNumber].innerHTML = '_'
+       } else generate()
+  }
+  
+})
