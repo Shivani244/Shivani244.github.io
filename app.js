7376613914f35290ff83167285345e9a85d52265
Shivani244 <43355324+Shivani244@users.noreply.github.com>
2022-03-22 09:10:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gridDisplay = document.querySelector('.grid')
     const resultDisplay = document.getElementById(' result')
-  const width = 5
-  const height = 6
+    const keyboard = document.querySelector('.key-container')
+    const keys = [
+        'Q',
+        'W',
+        'E',
+        'R',
+        'T',
+        'Y',
+        'U',
+        'I',
+        'O',
+        'P',
+        'A',
+        'S',
+        'D',
+        'F',
+        'G',
+        'H',
+        'J',
+        'K',
+        'L',
+        'ENTER',
+        'Z',
+        'X',
+        'C',
+        'V',
+        'B',
+        'N',
+        'M',
+    ]
+    const handleClick = () => {
+        console.log('clicked')
+    }
+    
+    const width = 5
+  //const height = 2
+  const height1 = 6
   let squares = []
   
-  
   function createBoard() {
-      for (let i=0; i < width*height; i++){
-          square = document.createElement('div')
-          square.innerHTML = ''
-          gridDisplay.appendChild(square)
-          squares.push(square)
-     
-      }
-      generate()  
-  }
-  
-  createBoard()
+    for (let i=0; i < width*height1; i++){
+        let square = document.createElement('div')
+        square.innerHTML = ''
+        gridDisplay.appendChild(square)
+        squares.push(square)
+   
+    } 
+    generate()
+}
+createBoard()
 
-  
-  //random grid
+ //random grid
   
   function generate() {
       let randomNumber = Math.floor(Math.random() * 20 + 10)
-       if (squares[randomNumber].innerHTML == '' ) {
-          squares[randomNumber].innerHTML = '_'
+       if (squares[randomNumber].innerHTML == '') {
+          squares[randomNumber].innerHTML = '-'
        } else generate()
   }
+
+
+  
+  keys.forEach(key => {
+      const buttonElement = document.createElement('button')
+      buttonElement.textContent = key
+      keyboard.append(buttonElement)
+      buttonElement.addEventListener('click', handleClick)
+  })
   
 })
+
+
